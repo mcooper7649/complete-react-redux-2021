edd3318b94e76d307666a52be69bd542ef3d17b0
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-07-31 15:10:15 -0400

    class and arrow functions, event binding exercise

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './components/card-list/card-list.component';
 import './App.css'
+import SearchBox from './components/search-box.component';
 
 class App extends Component {
     constructor(){
@@ -9,6 +10,12 @@ class App extends Component {
            monsters:[],
            searchField: ''
         }
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(e){
+      this.setState({
+        searchField: e.target.value
+      })
     }
 
     componentDidMount(){
@@ -26,11 +33,9 @@ class App extends Component {
       const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
         return (
             <div className='App'>
-            <input type="search"
+             <SearchBox 
             placeholder="search monsters..."
-            onChange={e => this.setState({
-              searchField: e.target.value
-            })}
+            handleChange={this.handleChange()}
              />
             <CardList monsters={filteredMonsters} />
             </div>

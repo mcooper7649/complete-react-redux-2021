aca52e7c57ef2fb9fc3ed5219788955129213065
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-08-01 12:41:55 -0400

    yarn build

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -34,7 +34,7 @@ class App extends Component {
         return (
             <div className='App'>
             <h1>Monsters Rolodex</h1>
-             <SearchBox 
+            <SearchBox 
             placeholder="search monsters..."
             handleChange={this.handleChange}
              />
